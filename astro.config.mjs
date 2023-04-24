6db00f50ade76b48ed9f882d8b6a7674a717ab17
Leo0114 <leonel0614@gmail.com>
2023-04-24 03:19:56 -0600

    deploy ghpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://myblog",
+  site: "https://Leo0114.github.io",
+  base: "/blog",
   integrations: [
     tailwind(),
     image({
